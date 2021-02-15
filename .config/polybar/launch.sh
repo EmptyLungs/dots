64ef84563b57999ce27d82e7c01e944c98ca09aa
Dmitry Pavlenko <dmitryrint@gmail.com>
2021-02-15 22:35:38 +0700

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
+DIR="$HOME/.config/polybar"
+
 killall -q polybar
 
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bar1 &
-polybar bar2 &
-
-echo "Bars launched..."
+polybar -q main -c "$DIR"/config.ini &
